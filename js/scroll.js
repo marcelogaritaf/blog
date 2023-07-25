22002417c7a4577a060c52e2c6b00cda5184c078
Marcelo Garita <mgaritaf@outlook.com>
2023-07-25 16:08:49 -0600

    adding styles and js scroll reveal

---
+++ js/scroll.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".menu", {
+  duration: 5000,
+  origin: "top",
+  distance: "-100px",
+});
+sr.reveal(".main-news img", {
+  duration: 5500,
+  origin: "left",
+  distance: "400px",
+});
+sr.reveal(".main-news .news", {
+  duration: 6500,
+  origin: "bottom",
+  distance: "400px",
+});
+sr.reveal(".secundary-news", {
+  duration: 6000,
+  origin: "right",
+  distance: "400px",
+});
+//paginas de la tienda
+sr.reveal(".more-news", {
+  duration: 6000,
+  origin: "bottom",
+  distance: "40px",
+});
